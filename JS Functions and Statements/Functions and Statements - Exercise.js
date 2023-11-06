3372b054ca29ea528fb906bee35e1cc8966ad574
ZhechoZhechev <mishomishev024@gmail.com>
2023-11-06 14:22:14 +0200

    Exercises

--- JS Functions and Statements/Functions and Statements - Exercise.js
+++ JS Functions and Statements/Functions and Statements - Exercise.js
@@ -40,6 +40,7 @@ console.log(charsToAsciiCodes("C", "#"));
 //04. Odd and Even Sum
 
 function sumOfOddAndEvenNumbers(number){
+    
     let numAsString = number.toString();
     let oddSum = 0;
     let evenSum = 0;
@@ -55,3 +56,113 @@ function sumOfOddAndEvenNumbers(number){
 }
 
 console.log(sumOfOddAndEvenNumbers(1000435));
+
+//05. Palindrome Integers
+
+function ifIsAPalindrome(arr){
+
+    arr.forEach(element => {
+        let reversedElement = parseInt(element.toString().split("").reverse().join(""));
+        element === reversedElement ? console.log("true") : console.log("false");
+    });
+}
+
+ifIsAPalindrome([32,2,232,1010]);
+
+//06. Password Validator
+
+function validatePassword(pass){
+    let valid = true;
+
+    if (pass.length < 6 || pass.length > 10) {
+        console.log("Password must be between 6 and 10 characters");
+        valid = false;
+    }
+
+    if (!/^[a-zA-Z0-9]+$/.test(pass)) {
+        console.log("Password must consist only of letters and digits");
+        valid = false;
+    }
+
+    let counter = 0;
+    charArray = pass.split("");
+    charArray.forEach(char =>{
+    let outcome = /^\d$/.test(char)
+    if (outcome === true) counter++;
+    });
+
+    if (counter < 2) {
+        console.log("Password must have at least 2 digits");
+        valid = false;
+    }
+
+    if (valid === true){
+        console.log("Password is valid");
+    }
+}
+
+validatePassword("logIn");
+
+//07. NxN Matrix
+
+function printMatrix(n) {
+    for (let i = 1; i <= n; i++) {
+        let row = '';
+        for (let j = 1; j <= n; j++) {
+            row += n + ' ';
+        }
+        console.log(row);
+    }
+}
+
+printMatrix(3);
+
+//08. Perfect Number 
+
+function isItPerfect(num){
+    sum = 0;
+    for (let i = 1; i < num; i++) {
+        if (num % i === 0) {
+            sum += i;
+        }
+    }
+    if (sum === num) {
+        console.log("We have a perfect number!");
+    } else {
+        console.log("It's not so perfect.");
+    }
+}
+
+isItPerfect(1236498);
+
+//09. Loading Bar
+
+function createLoadingBar(num){
+    let firstCharRepeats = num /10;
+    let secondCharRepeats = 10 - firstCharRepeats;
+    if (num !== 100) {
+        console.log(`${num}% [${'%'.repeat(firstCharRepeats)}${'.'.repeat(secondCharRepeats)}]`);
+        console.log("Still loading...");
+    } else {
+        console.log("100% Complete!")
+        console.log(`${num.repeat(firstCharRepeats)}`);
+    }
+}
+
+createLoadingBar(50);
+
+//10. Factorial Division
+
+function factorialDevision(num1, num2){
+    let num1Sum = 1;
+    for (let i = 1; i <= num1; i++) {
+        num1Sum *= i;
+    }
+    let num2Sum = 1;
+    for (let i = 1; i <= num2; i++) {
+        num2Sum *= i;
+    }
+    return (num1Sum / num2Sum).toFixed(2);
+}
+
+console.log(factorialDevision(6, 2));
