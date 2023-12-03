323d446e23c542dc4878ae92ed877ca122ee5963
ZhechoZhechev <mishomishev024@gmail.com>
2023-12-03 19:14:08 +0200

    Modified Solution

--- JS HTTP and REST/HTTP and REST  Lab/05.Book-library/app.js
+++ JS HTTP and REST/HTTP and REST  Lab/05.Book-library/app.js
@@ -1,132 +1,135 @@
+const loadBooksBtn = document.querySelector("#loadBooks");
+const submitBtn = document.querySelector("#form button");
 const tableBody = document.querySelector("tbody");
-const apiURL = "http://localhost:3030/jsonstore/collections/books"
-const titleInput = document.querySelector("input[name=title]");
-const authorInput = document.querySelector("input[name=author]");
-let allBooksData;
-let bookIDForEditing;
-let submitBtn = document.querySelector("#form button");
 
-function attachEvents() {
-  document.querySelector("#loadBooks").addEventListener("click", loadAllBooks);
-  submitBtn.addEventListener("click", handleSubmission)
+loadBooksBtn.addEventListener("click", displayBooks);
+submitBtn.addEventListener("click", handleButtonClick);
+tableBody.addEventListener("click", handleButtonClick);
 
+
+function handleButtonClick(e) {
+  const target = e.target;
+
+  if (target.tagName === "BUTTON" && target.textContent === "Delete") {
+    deleteBook(target);
+  } else if (target.tagName === "BUTTON" && target.textContent === "Edit"){
+    updateBook(target);
+  } else if (target.tagName === "BUTTON" && target.textContent === "Submit"){
+    saveBook();
+  } else if (target.tagName === "BUTTON" && target.textContent === "Save") {
+    saveNewBookinfo(target);
+  }
 }
 
-async function loadAllBooks() {
-  const booksData = await fetch(apiURL);
-  allBooksData = await booksData.json();
-  tableBody.innerHTML = "";
-  Object.entries(allBooksData).map(([_id, book]) => {
-    const row = createTableRow(_id, book);
-    tableBody.appendChild(row);
+async function updateBook(e) {
+  changeToUpdateForm();
+  fillInUpdateForm(e);
+}
+
+async function saveNewBookinfo(e){
+  const bookId = e.dataset.id;
+  const titleEl = document.querySelector("input[name=title]");
+  const authorEl = document.querySelector("input[name=author]");
+  const title = titleEl.value;
+  const author = authorEl.value;
+
+  await fetch(`http://localhost:3030/jsonstore/collections/books/${bookId}`, {
+    method: "PUT",
+    body: JSON.stringify({ title, author })
   })
 
-  clearInput()
+  clearInputs(titleEl, authorEl);
+  displayBooks();
 }
 
+async function deleteBook(e) {
+  const bookId = e.dataset.id;
 
-function createTableRow(_id, bookInfo) {
-  let newRow = document.createElement("tr");
-
-  let title = document.createElement("td");
-  title.textContent = bookInfo.title;
-  newRow.appendChild(title);
-
-  let author = document.createElement("td");
-  author.textContent = bookInfo.author;
-  newRow.appendChild(author);
-
-
-  let newDataCell = document.createElement("td");
-  newDataCell.appendChild(createActionBtn("Edit", _id));
-  newDataCell.appendChild(createActionBtn("Delete", _id));
-  newRow.appendChild(newDataCell);
-
-  return newRow
+  await fetch(`http://localhost:3030/jsonstore/collections/books/${bookId}`, {
+    method: "DELETE"
+  })
+  displayBooks();
 }
 
-function createActionBtn(text, _id) {
-  let newActionBtn = document.createElement("button")
-  newActionBtn.textContent = text
-  newActionBtn.setAttribute("data-id", _id)
+function saveBook() {
+  const titleEl = document.querySelector("input[name=title]");
+  const authorEl = document.querySelector("input[name=author]");
 
-  if (text === "Edit") newActionBtn.addEventListener("click", editBookInfo);
-  else newActionBtn.addEventListener("click", deleteBook);
+  const title = titleEl.value;
+  const author = authorEl.value;
 
-  return newActionBtn
-}
 
-function editBookInfo(event) {
-  submitBtn.textContent = "Save";
-  bookID = event.target.dataset.id
-  console.log(bookID)
-  bookIDForEditing = bookID;
-
-  titleInput.value = allBooksData[bookID].title
-  authorInput.value = allBooksData[bookID].author
-}
-
-function deleteBook(event) {
-  let bookID = processCurrentRow(event.target)
-  fetch(`${apiURL}/${bookID}`, { method: "DELETE" });
-
-}
-
-function handleSubmission(event) {
-  event.preventDefault()
-  const isEditing = submitBtn.textContent === "Save"
-
-  if (isEditing) {
-
-    let editedBook = handleBookData();
-    editedBook._id = bookIDForEditing
-
-    if (!editedBook) return;
-
-    fetch(`${apiURL}/${bookIDForEditing}`, {
-      method: "PUT",
-      body: JSON.stringify(editedBook),
-      headers: {
-        "Content-Type": "application/json"
-      }
-    })
-
-    submitBtn.textContent = "Submit";
-    clearInput()
-    return
+  if (!title || !author) {
+    return;
   }
 
-  let newBook = handleBookData()
-
-  if (!newBook) return;
-  fetch(apiURL, {
+  fetch("http://localhost:3030/jsonstore/collections/books", {
     method: "POST",
-    body: JSON.stringify(newBook),
-    headers: {
-      "Content-Type": "application/json"
-    }
+    body: JSON.stringify({ title, author })
   });
 
-  clearInput()
-
-
+  clearInputs(titleEl, authorEl);
+  displayBooks();
 }
 
-function handleBookData(book) {
-  let BookTitle = titleInput.value;
-  let BookAuthor = authorInput.value;
+function fillInUpdateForm(target) {
+  const title =
+  target.parentElement.parentElement.querySelector(
+      "td:first-child"
+    ).textContent;
+  const author =
+  target.parentElement.parentElement.querySelector(
+      "td:nth-child(2)"
+    ).textContent;
 
-  if (!BookAuthor || !BookTitle) return;
-
-  return {
-    title: BookTitle,
-    author: BookAuthor
-  }
+  document.querySelector('#form input[name="title"]').value = title;
+  document.querySelector('#form input[name="author"]').value = author;
+  document
+    .querySelector("#form button")
+    .setAttribute("data-id", target.dataset.id);
 }
 
-function clearInput() {
+function changeToUpdateForm() {
+  let h3 = document.querySelector("#form h3");
+  h3.textContent = "Edit FORM";
+  let submitBtn = document.querySelector("#form button");
+  submitBtn.textContent = "Save";
+}
+
+function clearInputs(titleInput, authorInput) {
   titleInput.value = "";
   authorInput.value = "";
 }
 
-attachEvents();
+async function displayBooks() {
+  tableBody.innerHTML = "";
+  let info = await fetch("http://localhost:3030/jsonstore/collections/books");
+  let booksInfo = await info.json();
+
+  Object.entries(booksInfo).map(([id, book]) => {
+    tableBody.appendChild(createTableRow(id, book));
+  });
+}
+
+function createTableRow(id, book) {
+  let row = document.createElement("tr");
+  let titelBox = document.createElement("td");
+  titelBox.textContent = book.title;
+  let authorBox = document.createElement("td");
+  authorBox.textContent = book.author;
+  let buttnosBox = document.createElement("td");
+  let editBtn = document.createElement("button");
+  editBtn.textContent = "Edit";
+  editBtn.setAttribute("data-id", id)
+  let deleteBtn = document.createElement("button");
+  deleteBtn.textContent = "Delete";
+  deleteBtn.setAttribute("data-id", id)
+  buttnosBox.appendChild(editBtn);
+  buttnosBox.appendChild(deleteBtn);
+
+  row.appendChild(titelBox);
+  row.appendChild(authorBox);
+  row.appendChild(buttnosBox);
+
+  return row;
+}
